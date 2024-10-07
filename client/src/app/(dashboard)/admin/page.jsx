6b17e4fe9245434d83a5d1b4ea414267f7fa11a0
mdazmi28 <md.azmi.siddique@gmail.com>
2024-10-07 10:45:30 +0600

    Circular Chart Complete

--- client/src/app/(dashboard)/admin/page.jsx
+++ client/src/app/(dashboard)/admin/page.jsx
@@ -1,23 +1,40 @@
 // import React from 'react';
 
 
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   const userTypes = ["student", "teacher", "parents", "staffs"];
   return (
-    <div className="flex flex-col gap-2 md:flex-row">
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* ADMIN */}
-      <div className="w-full lg:w-2/3">
+      {/* Left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
           {userTypes.map((type, index) => (
             <UserCard key={index} type={type} />
           ))}
         </div>
+        {/* Middle Chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count Chart */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+          <CountChart/>
+
+          </div>
+          {/* Attendance Chart Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom Chart */}
+        <div>
+
+        </div>
         <div>
           hello
         </div>
       </div>
+      {/* Right */}
       <div className="w-full lg:w-1/3 bg-blue-600">r</div>
     </div>
   );
--- client/src/components/CountChart.jsx
+++ client/src/components/CountChart.jsx
@@ -4,109 +4,71 @@ import Image from "next/image";
 import {
   RadialBarChart,
   RadialBar,
-  Legend,
-  PolarRadiusAxis,
+  // Legend,
   PolarAngleAxis,
+  ResponsiveContainer,
 } from "recharts";
 
+// Sample Data
 const data = [
   {
-    name: "18-24",
-    uv: 18,
-    pv: 2400,
+    name: "Total",
+    count: 125,
+    fill: "white",
+  },
+  {
+    name: "Boys",
+    count: 75,
     fill: "#8884d8",
   },
   {
-    name: "25-29",
-    uv: 26,
-    pv: 4567,
-    fill: "#83a6ed",
-  },
-  {
-    name: "30-34",
-    uv: 31,
-    pv: 1398,
-    fill: "#8dd1e1",
-  },
-  {
-    name: "35-39",
-    uv: 35.22,
-    pv: 9800,
-    fill: "#82ca9d",
-  },
-  {
-    name: "40-49",
-    uv: 45.63,
-    pv: 3908,
-    fill: "#a4de6c",
-  },
-  {
-    name: "50+",
-    uv: 51.63,
-    pv: 4800,
-    fill: "#d0ed57",
-  },
-  {
-    name: "unknown",
-    uv: 99.67,
-    pv: 4800,
-    fill: "#ffc658",
+    name: "Girls",
+    count: 50,
+    fill: "pink",
   },
 ];
 
-//   const style = {
-//     top: 0,
-//     left: 350,
-//     lineHeight: "24px",
-//   };
-
 const CountChart = () => {
   return (
-    <div>
+    <div className="bg-white rounded-xl h-full w-full p-4">
       {/* Title */}
-      <div>
+      <div className="flex justify-between items-center">
         <h1>Student</h1>
-        <Image/>
+        <Image src="/moreDark.png" alt="More" width={20} height={20} />
       </div>
+
       {/* Chart */}
-      <div>
-        <RadialBarChart
-          width={500}
-          height={300}
-          cx={150}
-          cy={150}
-          innerRadius={20}
-          outerRadius={140}
-          barSize={10}
-          data={data}
-        >
-          <RadialBar
-            // minAngle={15}
-            // label={{ position: "insideStart", fill: "#fff" }}
-            background
-            // clockWise
-            dataKey="uv"
-            // max
-          />
-          <PolarAngleAxis
-            tick={false}
-            domain={[0, 100]}
-            type="number"
-            // @ts-expect-error this prop works but isn't correct in TS types
-            reversed
-          />
-          <Legend
-            iconSize={10}
-            width={120}
-            height={140}
-            layout="vertical"
-            verticalAlign="middle"
-            wrapperStyle={style}
-          />
-        </RadialBarChart>
+      <div className="relative w-full h-[75%]">
+        <ResponsiveContainer>
+          <RadialBarChart
+            cx="50%"
+            cy="50%"
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
+            data={data}
+          >
+            <RadialBar background dataKey="count" />
+            <PolarAngleAxis tick={false} domain={[0, 100]} type="number" reversed />
+          </RadialBarChart>
+        </ResponsiveContainer>
+        {/* <Image src="maleFemale.png" alt="" height={20} width={30} className="absolute"/> */}
+        <Image src="/maleFemale.png" alt="image" height={50} width={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
       </div>
-      {/* Bottom */}
-      <div></div>
+
+      {/* Bottom Section */}
+      <div className="flex justify-center gap-16 mt-4">
+        <div className="flex flex-col gap-1 items-center">
+          <div className="w-5 h-5 rounded-full bg-blue-200"></div>
+          <h1 className="font-bold">1234</h1>
+          <h1 className="text-xs text-gray-300">Category A</h1>
+        </div>
+        <div className="flex flex-col gap-1 items-center">
+          <div className="w-5 h-5 rounded-full bg-pink-200"></div>
+          <h1 className="font-bold">1234</h1>
+          <h1 className="text-xs text-gray-300">Category B</h1>
+        </div>
+      </div>
     </div>
   );
 };
